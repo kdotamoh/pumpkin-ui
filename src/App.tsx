85c987d02f389f391b2e12281d4bab4212bcd23c
kdotamoh <i.be.your.strychnine@gmail.com>
2020-04-21 18:00:11 +0000

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Alert } from "antd";
+
+function App() {
+  return (
+    <Alert message="Hi, I'm from Ant Design" type="success" banner closable />
+  );
+}
+
+export default App;
