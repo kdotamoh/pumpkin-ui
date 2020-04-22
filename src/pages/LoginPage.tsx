0bf0cf6a7508d0c9e4a9e7200f3f803a60bf67bc
kdotamoh <i.be.your.strychnine@gmail.com>
2020-04-22 23:14:48 +0000

    button css

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,27 +5,40 @@ export const LoginPage: FunctionComponent = () => {
   return (
     <div className="login-page">
       <div className="login-page__main">
-        <h1>ADMIN PORTAL</h1>
+        <div className="login-page__navigation">
+          <div>logo</div>
+          <a href="#/">Home</a>
+        </div>
         <form className="login-form">
+          <h1>ADMIN PORTAL</h1>
           <h3>LOGIN</h3>
 
           <label htmlFor="email">E-Mail</label>
-          <input type="text" name="email" />
+          <input type="text" name="email" className="login-form__input" />
 
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            className="login-form__input"
+          />
 
           <label htmlFor="remember-me">
-            <input type="checkbox" name="rememberMe" id="" />
+            <input
+              type="checkbox"
+              name="rememberMe"
+              className="login-form__input"
+              id=""
+            />
             Remember me
           </label>
 
-          <button type="submit">Login</button>
+          <button className="button button--primary" type="submit">
+            Login
+          </button>
         </form>
       </div>
-      <div>
-        <img className="login-page__image" src="" alt="Login" />
-      </div>
+      <div className="login-page__image" />
     </div>
   );
 };
